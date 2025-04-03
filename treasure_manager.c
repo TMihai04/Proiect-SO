4450ab596b7f271a31cbdf3559df203297b12624
TMihai04 <tmihai0404@gmail.com>
2025-04-03 12:21:36 +0300

    started writing code

--- treasure_manager.c
+++ treasure_manager.c
@@ -1,3 +1,14 @@
 /*
-  Main application file for the Treasure hunt Project.
+  Main application file for the Treasure hunt Project. More information in the README.md file
+
+  Project done by Mihai Toderasc - 2025
  */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  return 0;
+}
+
+
