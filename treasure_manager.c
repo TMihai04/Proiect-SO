65c82fcff92241d39ab736e63b808561f2e5bbf9
TMihai04 <tmihai0404@gmail.com>
2025-04-06 22:06:17 +0300

    Done the --add command Works on adding multiple entries at the same time as well.

--- treasure_manager.c
+++ treasure_manager.c
@@ -21,6 +21,9 @@
 //#define MAX_TRJID_LEN 32 // maximum length of treasure id
 #define MAX_TRJDESC_LEN 64 // maximum length of treasure description
 #define MAX_USRID_LEN 32 // maximum length of user id
+#define MAX_TRJ_CNT 256 // maximum number of treasure entries
+
+#define MAX_BUFFER_READ 128 // maximum number of bytes that can be read at once at stdin
 
 /*
   maximum values on bits (unsigned):
@@ -50,17 +53,21 @@ struct option_t {
 
 // structure with (treasure id, starting byte) pair
 // sitauated at the start of the treasures file for optimisation purposes
+/*
 struct header_t {
   //char trj_id[MAX_TRJID_LEN];
-  uint8 trj_id;
-  uint16 st_byte;
+  uint8_t entries_cnt;
+  uint8_t trj_ids[MAX_TRJ_CNT];
+  uint16_t st_bytes[MAX_TRJ_CNT];
 };
+ */
+// i decided i will go for simplicity
 
 // structure containing the treasure details
 // makes for every actual entry in the treasures file
 struct trj_data_t {
   //char trj_id[MAX_TRJID_LEN];
-  uint8 trj_id;
+  uint8_t id;
   char usr_id[MAX_USRID_LEN];
   float lati, longi;
   char desc[MAX_TRJDESC_LEN];
@@ -96,7 +103,7 @@ void add_treasure(char* hunt_id) {
   strcat(trj_path, TRJFILE_NAME); // whole path for treasure file
   
   // if directory doesn't exist, create it and initilaise it with respective files
-  if(!(stat(dir_path, &sb) && S_ISDIR(sb.st_mode))) {
+  if(stat(dir_path, &sb) && !S_ISDIR(sb.st_mode)) {
     printf("The hunt \'%s\' is not currently happening. Making it happen...\t", hunt_id);
     mkdir(dir_path, 0777); // make a dir with all permisions (?)
     printf("Done.\n");
@@ -111,15 +118,13 @@ void add_treasure(char* hunt_id) {
 
     // log the directory creation
     wr_log[0] = '\0';
-    strcat(wr_log, "Hunt \'");
-    strcat(wr_log, hunt_id);
-    strcat(wr_log, "\' created.\n");
-    write(log_fd, wr_log, strlen(wr_log) + 1);
+    sprintf(wr_log, "Hunt \'%s\' was created", hunt_id);
+    write(log_fd, wr_log, (strlen(wr_log) + 1) * sizeof(char));
 
     // log the log file creation
     wr_log[0] = '\0';
     strcat(wr_log, "Log file created.\n");
-    write(log_fd, wr_log, strlen(wr_log) + 1);
+    write(log_fd, wr_log, (strlen(wr_log) + 1) * sizeof(char));
 
     printf("Creating space for treasures to be stored...\t");
     // create the treasures file and give r/w permissions
@@ -127,19 +132,139 @@ void add_treasure(char* hunt_id) {
       printf("Error creating treasure file.\n");
       exit(2);
     }
+    
     close(trj_fd);
     printf("Done.\n");
 
     // log treasure file creation
     wr_log[0] = '\0';
     strcat(wr_log, "Treasures file created.\n");
-    write(log_fd, wr_log, strlen(wr_log) + 1);
+    write(log_fd, wr_log, (strlen(wr_log) + 1) * sizeof(char));
     
     close(log_fd);
     printf("Logged initial operation.\n");
   }
 
+  // now that we are sure the required directory exists, we can add the entry
+
+  // open treasures file for reading and writing
+  if((trj_fd = open(trj_path, O_RDWR)) == -1) {
+    printf("Error retrieving treasures file.\n");
+    exit(3);
+  }
+
+  // read through the file and find the first unsuded id in range
+  uint8_t used_ids[MAX_TRJ_CNT] = { 0 };
+  struct trj_data_t curr_trj = { 0 };
   
+  while(read(trj_fd, &curr_trj, sizeof(struct trj_data_t))) {
+    used_ids[curr_trj.id]++;
+  }
+
+  uint8_t new_id = 0;
+  while(used_ids[new_id]) { new_id++; }
+  if(new_id >= MAX_TRJ_CNT) {
+    printf("This hunt already holds value beyond imagination. Try assigning this treasure to another hunt or remove one treasure from this hunt and retry the operation.\n");
+    exit(0);
+  }
+
+  char line[MAX_BUFFER_READ + 2]; // line read from stdin
+  struct trj_data_t new_entry = { 0 }; // entry to be added to file
+  new_entry.id = new_id;
+  
+  // prompt the user for information about the treasure entry
+  printf("\n -- Now serving treasure %d: --\n", new_id);
+  printf("Input the data as asked. In the case that the string inputed is longer than the limit shown, it will be truncated to fit the length.\n\n");
+  
+  // user id: 
+  printf("Who adds this treasure? (max %d characters)\n", MAX_USRID_LEN);
+  printf("user-id: ");
+
+  if(fgets(line, MAX_USRID_LEN, stdin) == NULL) {
+    printf("Error reading user-id.\n");
+    exit(4);
+  }
+
+  // flush stdin buffer if input is longer
+  if(line[strlen(line) - 1] != '\n') {
+    while(fgetc(stdin) != '\n') {}
+  }
+  
+  line[strlen(line) - 1] = '\0';
+  //printf("%s\n", line); -- debug
+  strcpy(new_entry.usr_id, line);
+
+  //printf("%s\n", new_entry.usr_id); // -- debug
+
+  // latitude
+  printf("X marks the spot? (2 floats)\n");
+  printf("latitude: ");
+  fscanf(stdin, "%f", &new_entry.lati);
+
+  // longitude
+  printf("longitude: ");
+  fscanf(stdin, "%f", &new_entry.longi);
+
+  // flush newline from last input
+  fgetc(stdin);
+
+  // description
+  printf("I fancy a good story... (max %d characters)\n", MAX_TRJDESC_LEN);
+  printf("Description: ");
+
+  if(fgets(line, MAX_TRJDESC_LEN, stdin) == NULL) {
+    printf("Error reading description.\n");
+    exit(5);
+  }
+
+  // flush stdin buffer if input is longer
+  if(line[strlen(line) - 1] != '\n') {
+    while(fgetc(stdin) != '\n') {}
+  }
+  
+  line[strlen(line) - 1] = '\0';
+  strcpy(new_entry.desc, line);
+
+  // value
+  printf("Here for the booty! (integer)\n");
+  printf("value: ");
+  fscanf(stdin, "%d", &new_entry.val);
+
+  // flush newline from last input
+  // helps if multiple treasures are added at the same time
+  fgetc(stdin);
+
+  //printf("%d\n%s\n%f\n%f\n%s\n%d\n", new_entry.id, new_entry.usr_id, new_entry.lati, new_entry.longi, new_entry.desc, new_entry.val); // -- debug
+
+  printf("Marking treasure down...\t");
+  if(write(trj_fd, &new_entry, sizeof(new_entry)) == -1) {
+    printf("Error writing treasure.\n");
+    exit(6);
+  }
+  printf("Done.\n");
+
+  //read(trj_fd, &new_entry, sizeof(new_entry)); // -- debug
+  //printf("%d\n%s\n%f\n%f\n%s\n%d\n", new_entry.id, new_entry.usr_id, new_entry.lati, new_entry.longi, new_entry.desc, new_entry.val); // -- debug
+  
+  close(trj_fd);
+
+  printf("Logging new changes...\t");
+  if((log_fd = open(log_path, O_APPEND | O_WRONLY)) == -1) {
+    printf("Error retrieving logs.\n");
+    exit(7);
+  }
+
+  wr_log[0] = '\0';
+  sprintf(wr_log, "User \'%s\' added treasure with index \'%d\'\n",
+	  new_entry.usr_id,
+	  new_entry.id);
+  if(write(log_fd, wr_log, (strlen(wr_log) + 1) * sizeof(char)) == -1) {
+    printf("Error updating logs.\n");
+    exit(8);
+  }
+  printf("Done.\n");
+    
+  close(log_fd);
 }
 
 int main(int argc, char** argv) {
@@ -194,6 +319,7 @@ int main(int argc, char** argv) {
   // use the obtained option code to do stuff
   switch(curr_option_code) {
   case ADD:
+    /*
     if(argc != 3) {
       if(argc > 3)
 	printf("Did not yet implement adding multiple treasures at a time. Use one argument\n");
@@ -203,8 +329,18 @@ int main(int argc, char** argv) {
       print_usage(argv[0]);
       exit(0);
     }
-    
+
     add_treasure(argv[2]); // adds a treasure to given hunt by id
+    */
+    if(argc < 3) {
+      printf("Option takes at least one argument <hunt_id>\n");
+      print_usage(argv[0]);
+      exit(0);
+    }
+
+    for(int i = 2; i < argc; i++) {
+      add_treasure(argv[i]);
+    }
     
     break;
   default:
